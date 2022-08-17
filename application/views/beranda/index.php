c5748b53f672f02a86aff5232a2984f65c252c66
Siswandito Putro Nugroho <siswanditopn@gmail.com>
2022-08-18 05:03:26 +0700

    Pindah lokasi file

---
+++ application/views/beranda/index.php
@@ -0,0 +1,8 @@
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Beranda</h1>
+  </div>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil doloribus saepe nostrum inventore vitae vel temporibus, incidunt voluptas harum quod, consequatur minima quisquam sed, alias eveniet cum quas expedita placeat nemo. Dolorum assumenda reprehenderit temporibus in ab quaerat consectetur aspernatur, iusto quia a corporis dolor est asperiores recusandae. Alias deleniti accusamus obcaecati, voluptates distinctio nobis animi est minima magnam quis, cumque labore voluptas vel possimus. Fugit eaque mollitia temporibus fugiat! Saepe animi ex, rerum obcaecati, autem voluptatem veniam cupiditate, consequatur dolores exercitationem rem consectetur iure atque illo aut iste assumenda dolore nam. Ullam blanditiis impedit excepturi natus numquam, ipsum sed!
+  </p>
+</main>
