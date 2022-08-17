cb3de7c3a2ae90858ebd11f11ac3ec74fbeff3d5
Siswandito Putro Nugroho <siswanditopn@gmail.com>
2022-08-18 05:04:15 +0700

    Panggil view beranda di controller

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -4,6 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Beranda extends CI_Controller {
   public function index()
 	{
-		$this->load->view('index');
+		$this->load->view('template/head');
+		$this->load->view('template/body_header');
+		$this->load->view('template/body_menu');
+    $this->load->view('beranda/index');
+		$this->load->view('template/footer');
 	}
 }
