14894818266634828a6cf9b4e8f23418199c85c7
Siswandito Putro Nugroho <siswanditopn@gmail.com>
2022-08-18 05:59:34 +0700

    Hapus class active untuk menu

--- application/views/template/body_menu.php
+++ application/views/template/body_menu.php
@@ -4,7 +4,7 @@
       <div class="position-sticky pt-3">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?= base_url('beranda') ?>">
+            <a class="nav-link" aria-current="page" href="<?= base_url('beranda') ?>">
               <i class="fa-fw fa-solid fa-gauge-high"></i>
               Beranda
             </a>
