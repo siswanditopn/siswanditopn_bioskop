a19bbd21cc459757a49983126aba42087291f109
Siswandito Putro Nugroho <siswanditopn@gmail.com>
2022-08-18 05:03:52 +0700

    Template view

---
+++ application/views/template/body_menu.php
@@ -0,0 +1,44 @@
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="<?= base_url('beranda') ?>">
+              <i class="fa-fw fa-solid fa-gauge-high"></i>
+              Beranda
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('kotakabupaten') ?>">
+              <i class="fa-fw fa-solid fa-location-dot"></i>
+              Kota/Kabupaten
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('bioskop') ?>">
+              <i class="fa-fw fa-solid fa-city"></i>
+              Bioskop
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('film') ?>">
+              <i class="fa-fw fa-solid fa-film"></i>
+              Film
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('jadwal') ?>">
+              <i class="fa-fw fa-solid fa-calendar-days"></i>
+              Jadwal
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('order') ?>">
+              <i class="fa-fw fa-solid fa-cart-shopping"></i>
+              Order
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
---
+++ application/views/template/head.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>SiswanditoPN - Bioskop</title>
+
+    <!-- Bootstrap Example -->
+    <link href="<?= base_url('assets'); ?>/css/dashboard.css" rel="stylesheet">
+
+    <!-- Fontawesome versi 6 -->
+    <script src="https://kit.fontawesome.com/900e4dbd61.js" crossorigin="anonymous"></script>
+  </head>
